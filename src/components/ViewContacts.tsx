ed6021330d77640542b9567803294f98ff11e152
John Tawiah <johnbaktawiah@gmail.com>
2020-09-04 10:03:14 +0000

    updated contact views

--- src/components/ViewContacts.tsx
+++ src/components/ViewContacts.tsx
@@ -1,42 +1,60 @@
 import React, { Component } from "react";
 import { Button, Layout, Card } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import ContactDetails from "./ContactDetails";
 
-export class ViewContacts extends Component {
+class ViewContacts extends Component {
   state = {
     contacts: [
-      {
-        firstName: "John",
-        lastName: "Tawiah",
-        handle: "@john",
-      },
-      { firstName: "Michael", lastName: "Blankson", handle: "@blank" },
+      { id: 1, firstName: "John", lastName: "Tawiah", handle: "@john" },
+      { id: 2, firstName: "Michael", lastName: "Blankson", handle: "@blank" },
+      { id: 3, firstName: "David", lastName: "Adams", handle: "@david" },
     ],
+    details: { id: 1, firstName: "", lastName: "", handle: "" },
+    modalState: false,
   };
+
+  handleClick = (e: any) => {
+    const itemId = e.target.id;
+    const details = this.state.contacts.find((list) => {
+      return list.id === +itemId;
+    });
+    this.setState({ details, modalState: true });
+  };
+
   render() {
     const { Header } = Layout;
-    const list = this.state.contacts.map((item, index: number) => (
-      <Card className="contact-list">
-        <ul key={index}>
-          <li className="contact">
-            <p>
-              <strong>
-                {item.firstName} {item.lastName}
-              </strong>
-            </p>
-          </li>
-        </ul>
-      </Card>
-    ));
+    const list = this.state.contacts.map((item, index: number) => {
+      return (
+        <li key={index}>
+          <Card className="contact-list">
+            <div
+              className="contact"
+              id={item.id.toString()}
+              onClick={this.handleClick}
+            >
+              {item.firstName} {item.lastName}
+            </div>
+          </Card>
+        </li>
+      );
+    });
+    // console.log(this.state.details);
+
     return (
       <div>
         <Header className="contact-header">
           <h1>Contact app</h1>
         </Header>
-        <div className="page-title">
-          <h2>Contact List</h2>
+        <div className="list-container">
+          <ul> {list}</ul>
+          <div className="contact-details">
+            <ContactDetails
+              contact={this.state.details}
+              visible={this.state.modalState}
+            />
+          </div>
         </div>
-        <div className="list"> {list}</div>
         <div className="add-contact">
           <Button className="btn">
             <PlusOutlined className="plus" />
